69492d5959ba651cb0432fc89438ec38aebb034e
Bruno Gabriel <brunomunizlourenco@gmail.com>
2024-07-03 13:06:52 -0300

    A janela principal da interface foi feita, mas ainda falta integra-la ao resto do programa e criar o resto das interfaces

--- AddAnkiCards/Interface/InterfaceMain.py
+++ AddAnkiCards/Interface/InterfaceMain.py
@@ -1,51 +1,91 @@
 import customtkinter as ctk
 
 
-class mainAddCards:
-    """
-    Classe que representara e chamara todas as janelas
-    """
+class WindowMain(object):
+    def __init__(self, dbConnect, Logger) -> None:
+        '''Classe que representa as janelas do programa'''
+        #
+        # Iniciamos a janela
+        self.Master = ctk.CTk()
+        self.Master.resizable(0, 0)
+        #
+        # Definimos a cor do tema e o titulo
+        self.Master.title('Add Anki Cards')
+        self.colorTheme = '#009951'
+        #
+        # Fazemos o titulo da parte do ingles
+        self.LabelEnglish = ctk.CTkLabel(
+            self.Master, text='English', font=('Arial', 30)
+        )
+        self.LabelEnglish.grid(row=0, column=0, columns=2, padx=5, pady=5)
+        #
+        # Coletamos os dados dos cartoes e mostramos na janela
+        # TODO: fazer uma funcao que busca as informacao no banco de dados
+        InfoEnglish = ['Added Notes: n', 'Stored Notes: n', 'All Notes: n']
+        self.labelEnglishInfo = ctk.CTkLabel(
+            self.Master,
+            text=f'{InfoEnglish[0]:>} | ' +
+            f'{InfoEnglish[1]:<}\n{InfoEnglish[2]:^}',
+        )
+        self.labelEnglishInfo.grid(row=1, column=0, columns=2, padx=5, pady=5)
+        #
+        # Fazemos os botoes que vao adicionar e fazer os cartoes de ingles
+        self.btnEnglishAdd = ctk.CTkButton(
+            self.Master,
+            height=70,
+            text='Add Cards',
+            fg_color=self.colorTheme,
+        )
+        self.btnEnglishAdd.grid(row=2, column=0, padx=5, pady=5)
+        #
+        #
+        self.btnEnglishMake = ctk.CTkButton(
+            self.Master,
+            height=70,
+            text='Make Cards',
+            fg_color=self.colorTheme
+        )
+        self.btnEnglishMake.grid(row=2, column=1, padx=5, pady=5)
+        #
+        # Fazemos a divisoria
+        ctk.CTkCanvas(self.Master, height=0, width=300).grid(
+            row=3, column=0, columns=2, pady=5)
+        #
+        # E fazemos a mesma coisa para as opcoes e informacoes de informatica
+        self.LabelMath = ctk.CTkLabel(
+            self.Master, text='Math', font=('Arial', 30)
+        )  # titulo
+        self.LabelMath.grid(row=4, column=0, columns=2, padx=5, pady=5)
+        #
+        # Informacoes
+        # TODO: fazer uma funcao que busca as informacao no banco de dados
+        InfoMath = ['Added Cards: n', 'Stored Cards: n', 'All Cards: n']
+        self.labelMathInfo = ctk.CTkLabel(
+            self.Master,
+            text=f'{InfoMath[0]:>} | ' +
+            f'{InfoMath[1]:<}\n{InfoMath[2]:^}',
+        )
+        self.labelMathInfo.grid(row=5, column=0, columns=2, padx=5, pady=5)
+        #
+        # Botoes
+        self.btnMathAdd = ctk.CTkButton(
+            self.Master,
+            height=70,
+            text='Add Cards',
+            fg_color=self.colorTheme,
+        )
+        self.btnMathAdd.grid(row=6, column=0, padx=5, pady=5)
+        #
+        #
+        self.btnMathMake = ctk.CTkButton(
+            self.Master,
+            height=70,
+            text='Make Cards',
+            fg_color=self.colorTheme
+        )
+        self.btnMathMake.grid(row=6, column=1, padx=5, pady=5)
+        self.Master.mainloop()
 
-    def __init__(self) -> None:
-        """
-        Ela, inicialmente, cria uma mini janela
-        com butoes que levam a criacao das outras
-        """
-        self.winStart = ctk.CTk()
-        self.winStart.title('Start Menu')
-        self.winStart.geometry('360x120')
-        self.winStart.resizable(0, 0)
 
-        NamePrograman = ctk.CTkLabel(
-            self.winStart,
-            text='Add Anki Cards\n' + "From: 'Estudando" + " e programando'",
-        ).grid(row=0, column=0, columns=2)
-        btnMakeMath = ctk.CTkButton(
-            self.winStart, text='Make Cards Math', command=self.makeWinMath
-        ).grid(row=1, column=0, padx=20, pady=10)
-        btnMakeIngles = ctk.CTkButton(
-            self.winStart,
-            text='Make Cards English',
-            command=self.makeWinEnglish,
-        ).grid(row=1, column=1, padx=20, pady=10)
-        btnMakeCardsAnki = ctk.CTkButton(
-            self.winStart, text='Add Cards In Anki', command=self.makeAddCards
-        ).grid(row=2, column=0, columns=2, pady=10)
-        self.winStart.mainloop()
-
-    def makeWinMath(self):
-        """
-        Funcao que cria a janela de matematica
-        """
-        winMat = ctk.CTk()
-        winMat.title('Make Cards Math')
-
-    def makeWinEnglish(self):
-        """
-        Funcao que cria a janela de ingles
-        """
-        winEnglish = ctk.CTk()
-        winEnglish.title('Make Cards English')
-
-
-Teste = mainAddCards()
+if __name__ == '__main__':
+    WindowMain(None, None)
